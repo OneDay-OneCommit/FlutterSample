b262c1f254cc080272b79c0f081ea3b9414774ee
juyeong.park <juyeong.park@hanilprotech.com>
2023-11-09 15:27:29 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/firstsample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firstsample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/user.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class User
+{
+  int id = 0;
+  String name = '';
+
+  User(int id_, String name_)
+  {
+    id = id_;
+    name = name_;
+  }
+}
