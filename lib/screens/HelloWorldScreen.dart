0fbace8f1b683caf891aa58895d6cc5972f57def
juyeong.park <qkqwnjdud@gmail.com>
2023-11-26 13:53:59 +0900

    Home Screen 생성자 오버로딩

--- lib/screens/HelloWorldScreen.dart
+++ lib/screens/HelloWorldScreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class HelloWorldScreen extends StatelessWidget {
+  const HelloWorldScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
