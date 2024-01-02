38a44744ce7691c6500d41c7b3e4b8a47945bffa
juyeong.park <qkqwnjdud@gmail.com>
2024-01-03 01:00:24 +0900

    feat : 버튼 기능 추가

--- lib/screens/blogScreen/BlogScreen.dart
+++ lib/screens/blogScreen/BlogScreen.dart
@@ -5,7 +5,9 @@ final homeURL = Uri.parse("https://blog.codefactory.ai");
 
 class BlogScreen extends StatelessWidget {
   // WebView 출력을 위한 컨트롤러를 설정한다.
-  WebViewController controller = WebViewController()..loadRequest(homeURL);
+  WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..loadRequest(homeURL);
 
   BlogScreen({super.key});
 
@@ -16,6 +18,13 @@ class BlogScreen extends StatelessWidget {
         backgroundColor: Colors.blue,
         titleTextStyle: TextStyle(color: Colors.white, fontSize: 24),
         title: Text("code factory"),
+        actions: [
+          IconButton(
+              onPressed: () {
+                controller.loadRequest(homeURL);
+              },
+              icon: Icon(Icons.home))
+        ],
       ),
       // WebView를 출력하기 위한 Widget을 body로 설정한다.
       body: WebViewWidget(
